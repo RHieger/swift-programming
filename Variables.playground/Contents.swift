74e45b9317df12614d7a309a57f59ea44c271ff3
Bobby <rhieger@nyc.rr.com>
2016-02-10 14:46:31 -0500

    Constants v. Vaiables--Variables.playground: pages 55-58

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let numberOfStoplights: Int = 4     // Explicit type declaration
+
+// Mixing types--this will throw error. It is therefore commented out.
+
+//numberOfStoplights += 2
+
+//numberOfStoplights += 2           // After numberOfStoplights converted
+                                    // to a constant, its value is no longer
+                                    // mutable, and thus, this line of code
+                                    // is commented out.
+
+var population: Int = 5422
+
+let townName: String = "Knowhere"
