e62e35e2ce3bfbbbb453c6a92a387206311d1674
Bobby <rhieger@nyc.rr.com>
2016-02-10 13:26:28 -0500

    Chapter 1 Bronze Challenge: Compleed Chapter 1

---
+++ myLastName.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+// store a string containig my last name
+
+var lastName =  "Hieger"
+
+// Print lastName to console.
+
+print(lastName)
