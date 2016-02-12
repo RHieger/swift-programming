53ec60d77adbf8cc6056ac4be55fd5c3a427b920
Bobby <rhieger@nyc.rr.com>
2016-02-12 02:01:35 -0500

    Chapter 4 Numbers--Maximum and Minimum Values for Int: page 78

---
+++ Numbers.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - Numbers
+
+// Import necessary framework
+
+import Cocoa
+
+// Show the maximum value that can be represented by Int Type.
+
+print("The maximum Int value is \(Int.max).")
+
+// Show the minimum value that can be represented by Int Type.
+
+print("The minimum Int value is \(Int.min).")
