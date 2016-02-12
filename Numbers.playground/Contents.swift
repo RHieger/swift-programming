0507d3a051e05b922f68f5abb2949422eb13e200
Bobby <rhieger@nyc.rr.com>
2016-02-12 02:22:24 -0500

    Chapter 4 Numbers--Maximum and Minimum Values for UInt and UInt32: page 81

--- Numbers.playground/Contents.swift
+++ Numbers.playground/Contents.swift
@@ -19,3 +19,23 @@ print("The maximum Int32 value is \(Int32.max).")
 // Show the minimum value that can be represented by Int32 Type.
 
 print("The minimum Int32 value is \(Int32.min).")
+
+// Show the maximum value that can be represented by the
+// Unsigned Int (UInt) Type.
+
+print("The maximum UInt value is \(UInt.max).")
+
+// Show the minimum value that can be represented by the
+// Unsigned Int (UInt) Type.
+
+print("The minimum UInt value is \(UInt.min).")
+
+// Show the maximum value that can be represented by the
+// Unsigned Int32 (UInt32) Type.
+
+print("The maximum UInt32 value is \(UInt32.max).")
+
+// Show the minimum value that can be represented by the
+// Unsigned Int32 (UInt32) Type.
+
+print("The minimum UInt32 value is \(UInt32.min).")
