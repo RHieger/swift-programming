02990e94b2dce0137d62394f6732603a3cc2977c
Bobby <rhieger@nyc.rr.com>
2016-02-12 02:08:24 -0500

    Chapter 4 Numbers--Maximum and Minimum Int32 Values: page 80

--- Numbers.playground/Contents.swift
+++ Numbers.playground/Contents.swift
@@ -11,3 +11,11 @@ print("The maximum Int value is \(Int.max).")
 // Show the minimum value that can be represented by Int Type.
 
 print("The minimum Int value is \(Int.min).")
+
+// Show the maximum value that can be represented by Int32 Type.
+
+print("The maximum Int32 value is \(Int32.max).")
+
+// Show the minimum value that can be represented by Int32 Type.
+
+print("The minimum Int32 value is \(Int32.min).")
