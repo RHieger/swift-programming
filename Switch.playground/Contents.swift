dc9cf61fd5362eab41ddb0a48de92f3d30956b1b
Bobby <rhieger@nyc.rr.com>
2016-02-19 00:53:13 -0500

    Chapter 5 Switch.playground--Build Error Message with fallthrough Statement: pages 101-102

--- Switch.playground/Contents.swift
+++ Switch.playground/Contents.swift
@@ -6,26 +6,18 @@ import Cocoa
 
 var statusCode: Int = 404
 
-var errorString: String         // Variable for error string
+var errorString: String = "The request failed with the error: "
 
 // statusCode will act as the value against which our comparison
 // will be made in the switch statement below
 
 switch statusCode   {
     
-case 400:
-    errorString = "Bad request"
-    
-case 401:
-    errorString = "Unauthorized"
-    
-case 403:
-    errorString = "Forbidden"
-    
-case 404:
-    errorString = "Not found"
+case 400, 401, 403, 404:
+    errorString = "There was something wrong with the request."
+    fallthrough
     
 default:
-    errorString = "None"
+    errorString += " Please review the request and try again."
     
 }   // end switch statusCode
