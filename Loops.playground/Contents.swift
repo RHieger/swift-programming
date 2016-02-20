f9383b81521477740371be012579d42711218056
Bobby <rhieger@nyc.rr.com>
2016-02-20 01:24:25 -0500

    Chapter 6 Loops--Created for in Loop: pages 122-124

---
+++ Loops.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - Loops
+
+import Cocoa
+
+var myFirstInt: Int = 0
+
+for i in 1...5  {
+    
+    ++myFirstInt    // increment myFirstInt
+    
+    print(myFirstInt)
+    
+}   // end for i in 1...5
