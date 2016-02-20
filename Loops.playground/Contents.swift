fa07919a29200d5a77e349b42d81510d0eefaee5
Bobby <rhieger@nyc.rr.com>
2016-02-20 03:22:37 -0500

    Chapter 6 Loops.playground--the Classic for Loop: page 132

--- Loops.playground/Contents.swift
+++ Loops.playground/Contents.swift
@@ -9,3 +9,12 @@ for case let i in 1...100 where i % 3 == 0  {
     print(i)
     
 }   // end for i in 1...100 where i % 3 == 0
+
+// Classic for loop:
+
+for var i = 1; i < 6; ++i   {
+    
+    ++myFirstInt
+    print(myFirstInt)
+    
+}   // end for var i = 1; i < 6; ++i
