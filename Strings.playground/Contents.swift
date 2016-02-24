698eb27e8195598b00e2720d6b0c33a9f5e42da7
Bobby <rhieger@nyc.rr.com>
2016-02-23 23:44:22 -0500

    Chapter 7 Strings.playground--Created String Constant and Mutable String: pages 145-146

---
+++ Strings.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - Strings: a sandbox for String functionality
+
+import Cocoa
+
+let playground = "Hello, playground!"
+
+// Make a mutable string:
+
+var mutablePlayground = "Hello, mutable playground"
