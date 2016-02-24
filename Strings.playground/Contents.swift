1b80e56fdb57fafe0af924292a55a110c91be89b
Bobby <rhieger@nyc.rr.com>
2016-02-24 00:03:46 -0500

    Chapter 7 Strings.playground--Concatenation, Decomposition of String to Character Type: pages 146-148

--- Strings.playground/Contents.swift
+++ Strings.playground/Contents.swift
@@ -7,3 +7,13 @@ let playground = "Hello, playground!"
 // Make a mutable string:
 
 var mutablePlayground = "Hello, mutable playground"
+
+// Concatenate exclamation point to mutablePlayground String:
+
+mutablePlayground += "!"
+
+for c: Character in mutablePlayground.characters {
+    
+    print("\(c)")
+    
+}   // end for c: Character in mutablePlayground.characters
