a58209cd82ed9cec66bde88d5e9201dad9a61b31
Bobby <rhieger@nyc.rr.com>
2016-03-14 00:51:42 -0400

    Chapter 8 Optionals.playground--Adding Conditional to Check if Optional is nil: pages 164-165

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -21,3 +21,31 @@ var errorCodeString2: String?   // Another error code String
 // Log this optional's nil value to console.
 
 print(errorCodeString2)
+
+// Adding a condition to errorCodeString to test whether a type is nil:
+
+if errorCodeString != nil    {
+    
+    let theError = errorCodeString!     // Forcibly unwrapped optional
+    
+    // Output theError to console.
+    
+    print(theError)
+    
+}   // end if
+
+// Adding a condition to errorCodeString2 to test whether a type is nil:
+
+if errorCodeString2 != nil  {
+    
+    let theError = errorCodeString2!
+    
+    // Output theError to console.
+    
+    print(theError)
+    
+}   else    {
+    
+    print(errorCodeString2)
+    
+}   // end if-else
