1192a2fbf2c79e97000b0bf700b0857bbc40feac
Bobby <rhieger@nyc.rr.com>
2016-03-15 01:33:49 -0400

    Chapter 9 Arrays.playground--Declaring Array, 2nd Syntax: page 186

--- Arrays.playground/Contents.swift
+++ Arrays.playground/Contents.swift
@@ -6,4 +6,8 @@ import Cocoa
 
 // 1st syntax:
 
-var bucketList: Array<String>
+//var bucketList: Array<String>     // Commented out to allow 2nd syntax
+
+// 2nd syntax:
+
+var bucketList: [String]
