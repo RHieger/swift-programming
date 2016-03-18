c3c928451af49829c73455f6a3ed04278fe7bf80
Bobby <rhieger@nyc.rr.com>
2016-03-18 02:13:02 -0400

    Chapter 10: Dictionary.playground--Populating a Dictionary: pages 208-212

---
+++ Dictionary.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - Chapter 10: Dictionaries
+
+import Cocoa
+
+// NOTE: The four declaration syntaxes for Dictionaries:
+//
+//       var dict1: Dictionary<String, Double> = [:]
+//
+//       Instantiates empty Dictionary object with
+//       Dictionary literal syntax.
+//
+//       var dict2 = Dictionary<String, Double>()
+//
+//       Instantiates empty Dictionary object with
+//       the default initializer.
+//
+//       var dict3: Dictionary[String:Double] = [:]
+//
+//       var dict4 = Dictionary[String:Double]()
+
+// Instantiate and populate a Dictionary using literal syntax:
+
+var movieRatings = [
+                    "Donnie Darko": 4,
+                    "Chunking Express": 5,
+                    "Dark City": 4
+                   ]
