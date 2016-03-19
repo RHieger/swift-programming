2e75c743a297a327f08a093adb73cc55b1fb4b1b
Bobby <rhieger@nyc.rr.com>
2016-03-19 17:52:28 -0400

    Chapter 11 Groceries.playground--Added Elements to Set: page 228

--- Groceries.playground/Contents.swift
+++ Groceries.playground/Contents.swift
@@ -5,3 +5,11 @@ import Cocoa
 // Instantiate a set called groceryBag.
 
 var groceryBag = Set<String>()
+
+// Add some elements to groceryBag:
+
+groceryBag.insert("Apples")
+
+groceryBag.insert("Oranges")
+
+groceryBag.insert("Pineapples")
