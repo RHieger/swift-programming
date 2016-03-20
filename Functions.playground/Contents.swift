9dfe8fe226d97aa31458da0cf221bf6b141dd2d9
Bobby <rhieger@nyc.rr.com>
2016-03-20 13:43:30 -0400

    Chapter 12 Functions.playground--Declared and Called personalGreeting(name: String): pages 243-244

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -13,3 +13,17 @@ func printGreeting()    {
 // Call printGreeting().
 
 printGreeting()
+
+
+// Declare a function that takes a parameter for user name
+// and prints a personal greeting to the console.
+
+func personalGreeting(name: String) {
+    
+    print("\nHello \(name), welcome to your playground.\n")
+    
+}   // end personalGreeting()
+
+// Call personalGreeting(name: String).
+
+personalGreeting("Bobby")
