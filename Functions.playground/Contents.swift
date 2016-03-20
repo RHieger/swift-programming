10f1b6985e11b952e6d7aae810affc5100068e10
Bobby <rhieger@nyc.rr.com>
2016-03-20 13:36:20 -0400

    Chapter 12 Functions.playground--Declare and Call printGreeting() Function: pages 241-242

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -1,3 +1,15 @@
 //: Playground - Functions: Chapter 12
 
 import Cocoa
+
+// Declare a function that prints a greeting to the console.
+
+func printGreeting()    {
+    
+    print("Hello, playground.")
+    
+}   // end printGreeting()
+
+// Call printGreeting().
+
+printGreeting()
