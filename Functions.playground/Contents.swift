ff7f962ecd06d93a345df2f8f241d5ee5edd8405
Bobby <rhieger@nyc.rr.com>
2016-03-31 17:17:02 -0400

    Chapter 12 Functions.playground--Added Parameter with Default Value: pages 261-262

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -18,15 +18,15 @@ printGreeting()
 // Declare a function that takes a parameter for user name
 // and prints a personal greeting to the console.
 
-//func personalGreeting(name: String) {
-//    
-//    print("\nHello \(name), welcome to your playground.\n")
-//    
-//}   // end personalGreeting()
-//
-//// Call personalGreeting(name: String).
-//
-//personalGreeting("Bobby")
+func personalGreeting(name: String) {
+    
+    print("Hello \(name), welcome to your playground.\n")
+    
+}   // end personalGreeting()
+
+// Call personalGreeting(name: String).
+
+personalGreeting("Bobby")
 
 // Declare a function that performs a division operation
 // and has two parameters num (numerator) and den
@@ -50,19 +50,19 @@ printGreeting()
 //       is commented out so that we may explore the same
 //       functionality using named parameters.
 
-func divisionDescription(forNumerator num: Double,
-                       andDenominator den: Double)  {
-    
-    // Log calculation to console.
-    
-    print("\(num) divided by \(den) equals \(num / den).\n")
-    
-}   // end divisionDescription(forNumerator num: Double,
-    //                       andDenominator den: Double)
-
-// Call function.
-
-divisionDescription(forNumerator: 9.0, andDenominator: 3.0)
+//func divisionDescription(forNumerator num: Double,
+//                       andDenominator den: Double)  {
+//    
+//    // Log calculation to console.
+//    
+//    print("\(num) divided by \(den) equals \(num / den).\n")
+//    
+//}   // end divisionDescription(forNumerator num: Double,
+//    //                       andDenominator den: Double)
+//
+//// Call function.
+//
+//divisionDescription(forNumerator: 9.0, andDenominator: 3.0)
 
 // Using a variadic parameter, that is, a parameter that takes
 // zero or more input values.
@@ -83,3 +83,31 @@ func printPersonalGreetings(names: String...)   {
 
 printPersonalGreetings("Alex", "Chris", "Drew", "Pat", "Bobby")
 
+// Using Default Parameter Values.
+
+// NOTE: I have commented out the implementation of divisionDescription()
+//       above so that I may demonstrate the same function with a default
+//       parameter value. Here the parameter withPunctuation is added. It
+//       is given a default value and so we needn't provide this parameter
+//       with a value. But we can. This adds a desirable flexibility to
+//       the method.
+
+func divisionDescription(forNumerator num: Double,
+                       andDenominator den: Double,
+              withPunctuation punctuation: String = ".")  {
+    
+    // Print calculation to console.
+    
+    print("\(num) divided by \(den) equals \(num / den)\(punctuation)\n")
+    
+}   // end divisionDescription()
+
+// Call function without final parameter.
+
+divisionDescription(forNumerator: 9.0, andDenominator: 3.0)
+
+// Call function with final parameter specified.
+
+divisionDescription(forNumerator: 9.0,
+                    andDenominator: 3.0,
+                    withPunctuation: "!")
