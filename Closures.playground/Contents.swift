77189bb04d9992844cc8996bc2de55c0dd6cbc0f
Bobby <rhieger@nyc.rr.com>
2016-04-07 20:32:00 -0400

    Chapter 13 Closures.playground--Sort Array using sortAscending Custom Method: page 274

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -5,3 +5,16 @@ import Cocoa
 // Instantiate an Array to hold counts of volunteers.
 
 var volunteerCounts = [1, 3, 40, 32, 2, 53, 77, 13]
+
+// Sort the array using a custom function.
+
+func sortAscending(i: Int, j: Int) -> Bool  {
+    
+    return i < j
+    
+}   // end sortAscending(i: Int, j: Int) -> Bool
+
+// Use the sortAscending(_i:, _) method as the parameter
+// of the built-in sort() method of Array.
+
+var sortedVolunteers = volunteerCounts.sort(sortAscending)
